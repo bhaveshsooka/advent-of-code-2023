c3ea956bfbe5b90cde46bf33bde14e29c9ecec54
Bhavesh Sooka <bsooka@gmail.com>
2023-12-02 18:52:35 +0200

    day 1 and 2 - both parts

--- bhavesh/rust/src/days/day01.rs
+++ bhavesh/rust/src/days/day01.rs
@@ -1,22 +1,85 @@
 use crate::{models::aoc_answer::AocAnswer, utils::get_question_data::get_question_data};
 
-pub async fn solve() -> AocAnswer<String> {
-    let input_data = get_question_data(2022, 1)
+// --- Day 1: Trebuchet?! ---
+
+// --------------------------------------------------------------------------------------
+// Boilerplate
+// --------------------------------------------------------------------------------------
+pub async fn solve() -> AocAnswer<i32> {
+    let input_data = get_question_data(2023, 1)
         .await
         .expect("Could not get Day 1 data");
 
-    let answer: AocAnswer<String> = AocAnswer {
+    let answer: AocAnswer<i32> = AocAnswer {
         day: 1,
+        sample_solution_part1: sample_solution_part1(),
+        sample_solution_part2: sample_solution_part2(),
         part1: part1(&input_data),
         part2: part2(&input_data),
     };
     return answer;
 }
 
-fn part1(input_data: &String) -> String {
-    String::len(&input_data).to_string()
+fn part1(input_data: &String) -> i32 {
+    return input_data
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|x| x.to_string())
+        .map(match_digits)
+        .map(calc_calibration_number)
+        .sum();
 }
 
-fn part2(_input_data: &String) -> String {
-    String::from("Hello part 2")
+fn part2(input_data: &String) -> i32 {
+    return input_data
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|x| x.to_string())
+        .map(replace_words_with_digits)
+        .map(match_digits)
+        .map(calc_calibration_number)
+        .sum();
+}
+
+fn sample_solution_part1() -> i32 {
+    let input_data = String::from("1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n");
+    part1(&input_data)
+}
+
+fn sample_solution_part2() -> i32 {
+    let input_data = String::from("two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen\n");
+
+    part2(&input_data)
+}
+
+// --------------------------------------------------------------------------------------
+// Actual solution
+// --------------------------------------------------------------------------------------
+fn match_digits(string: String) -> Vec<i32> {
+    string
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .map(|c| c.to_string())
+        .map(|c| c.parse::<i32>().unwrap())
+        .collect()
+}
+
+fn replace_words_with_digits(string: String) -> String {
+    string
+        .replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
+}
+
+fn calc_calibration_number(calibration_numbers: Vec<i32>) -> i32 {
+    let first: String = calibration_numbers[0].to_string();
+    let last: String = calibration_numbers[calibration_numbers.len() - 1].to_string();
+
+    (first + &last).parse().unwrap()
 }
--- bhavesh/rust/src/models/aoc_answer.rs
+++ bhavesh/rust/src/models/aoc_answer.rs
@@ -1,7 +1,12 @@
 use std::fmt::{self, Display};
 
-pub struct AocAnswer<T> {
+pub struct AocAnswer<T>
+where
+    T: Display,
+{
     pub day: i32,
+    pub sample_solution_part1: T,
+    pub sample_solution_part2: T,
     pub part1: T,
     pub part2: T,
 }
@@ -12,6 +17,8 @@ where
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "Day: {}", self.day)?;
+        writeln!(f, "Sample Solution Part 1: {}", &self.sample_solution_part1)?;
+        writeln!(f, "Sample Solution Part 2: {}", &self.sample_solution_part2)?;
         writeln!(f, "Part 1: {}", &self.part1)?;
         writeln!(f, "Part 2: {}", &self.part2)?;
         Ok(())
